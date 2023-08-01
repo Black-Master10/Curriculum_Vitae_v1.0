bd49305ba515171ef7f025dd476a27c1b0f3d325
Ing.Alex Martinez <ing.alex.martinez.sanchez@gmail.com>
2023-07-31 22:44:21 -0600

    Agregar archivos de proyecto.

---
+++ Curriculum_Vitae_v1.0/Controllers/MenuPrincipalController.cs
@@ -0,0 +1,60 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.IO;
+using System.Web.Mvc;
+
+namespace Curriculum_Vitae.Controllers
+{
+    public class MenuPrincipalController : Controller
+    {
+        // GET: MenuPrincipal
+        [HttpGet]
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Inicio(string txtQRCode)
+        {
+            ViewBag.txtqRCode = txtQRCode;
+
+            QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+            QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode(txtQRCode, QRCodeGenerator.ECCLevel.Q);
+            QRCode qRCode = new QRCode(qRCodeData);
+
+            System.Web.UI.WebControls.Image imgBarCode = new System.Web.UI.WebControls.Image();
+
+            imgBarCode.Height = 3;
+            imgBarCode.Width = 3;
+
+            using (Bitmap bitmap = qRCode.GetGraphic(3))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+
+                    ViewBag.imageBytes = ms.ToArray();
+                }
+            }
+
+            return View();
+        }
+        public ActionResult HistorialAcademico()
+        {
+            return View();
+        }
+        public ActionResult HistorialProfesional()
+        {
+            return View();
+        }
+        public ActionResult Descarga()
+        {
+            return View();
+        }
+        public ActionResult Contactame()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Curriculum_Vitae_v1.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Curriculum_Vitae_v1._0.MvcApplication" Language="C#" %>
